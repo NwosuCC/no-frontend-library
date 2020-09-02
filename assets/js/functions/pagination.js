121b2752993f9fa61e606b378bf0911c58dec5b9
NwosuCC <nwosu.cyprian.c@gmail.com>
2020-09-02 02:23:00 +0100

    NW-005-Feat: improved on index pagination layout

--- assets/js/functions/pagination.js
+++ assets/js/functions/pagination.js
@@ -64,7 +64,7 @@ const Pagination = (() => {
       }
     },
     loadLinks: (item) => {
-      _obj.el = Dom.el(`[data-pagination="${item}"]`);
+      _obj.el = Dom.getElement(`[data-pagination="${item}"]`);
       if (!_obj.el) {
         return;
       }
@@ -72,7 +72,7 @@ const Pagination = (() => {
       let {page, limit, pages, counts} = _obj.get();
 
       // Set current page items summary
-      Dom.els('[data-pg-count]', _obj.el).forEach(countElement => {
+      Dom.getElements('[data-pg-count]', _obj.el).forEach(countElement => {
         let key = Dom.getAttribute(countElement, 'data-pg-count');
         Dom.setText(countElement, Util.Object.getOrDefault(counts[key], 0));
         countElement.removeAttribute('data-pg-count');
@@ -80,7 +80,7 @@ const Pagination = (() => {
 
       // Set pages links
       let activeLinkSet = false;
-      Dom.els('[data-pg-link]', _obj.el).forEach(linkElement => {
+      Dom.getElements('[data-pg-link]', _obj.el).forEach(linkElement => {
         let key = Dom.getAttribute(linkElement, 'data-pg-link').substr(6);
         linkElement.removeAttribute('data-pg-link');
         if (_obj.disableLinkAsNonRequired(linkElement, key, page, pages)) {
@@ -99,7 +99,7 @@ const Pagination = (() => {
       });
 
       // Set Pagination-specific event handlers (e.g 'Per page' <select> 'onchange' event)
-      Dom.els('[data-pg-handler]', _obj.el).forEach(handlerElement => {
+      Dom.getElements('[data-pg-handler]', _obj.el).forEach(handlerElement => {
         Dom.setEventHandler(
           handlerElement, 'data-pg-handler', (key) => _obj[key]
         );
@@ -112,7 +112,7 @@ const Pagination = (() => {
       let newLimit = Util.String.asNumber(event.target.value);
       Routes.go(Routes.paginated({page: _obj.page, limit: newLimit}));
     },
-    // Populates 'Per page' <select> options
+    // Populates 'Per page' Select <option> elements
     getPgPerPageOptionsHtml: () => {
       const html = (num, selected) => {
         let selectedAttr = selected ? 'selected="selected"' : '';
@@ -139,17 +139,17 @@ const Pagination = (() => {
     html: () => {
       let el = `<div class="pagination-meta">
                   <div class="pagination-limit">
-                    <label for="pg-per-page" class="font-weight-bold">Per page: </label>
+                    <label for="pg-per-page" class="label-inline" style="font-size: 15px;">Per Page: </label>
                     <!-- On <select> change, call event handler Pagination:pageLimitChanged() -->
-                    <select id="pg-per-page" data-pg-handler="change:pageLimitChanged">
+                    <select id="pg-per-page" class="b b-g" data-pg-handler="change:pageLimitChanged">
                       {pgOptions}
                     </select>
                   </div>
                   <div class="pagination-summary">
-                    <strong>Items: </strong>
                     <span class="pg-count" data-pg-count="from">1</span>
-                    to <span class="pg-count" data-pg-count="to">1</span>
+                    - <span class="pg-count" data-pg-count="to">1</span>
                     of <span class="pg-count" data-pg-count="of">1</span>
+                    items
                   </div>
                 </div>
                 <div class="pagination">
